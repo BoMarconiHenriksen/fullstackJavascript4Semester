34689b8c1f8a301707b45d9de34aa53bd889a0c9
Bo Henriksen <bomarconi@gmail.com>
2018-09-01 09:54:46 +0200

    Ex. 2 Promises Es6.

---
+++ periode1/recapJavascript/promise/Ex2ChainingPromises.js
@@ -0,0 +1,63 @@
+/* 
+Now, Implement a method getPlanetforFirstSpeciesInFirstMovieForPerson(id){} which for id = 1 (Luke Skywalker) should log this info:
+
+Name: Luke Skywalker
+First film: The Empire Strikes Back
+First species: Yoda's species
+Homeworld for Specie: unknown
+
+*/
+const fetch = require("node-fetch");
+
+async function getPlanetforFirstSpeciesInFirstMovieForPersonAsync(id) {
+  try {
+    const URL = "https://swapi.co/api/people/";
+
+    const n = await fetch(URL + id).then(res => res.json());
+    const f = await fetch(n.films[0]).then(res => res.json());
+    const s = await fetch(f.species[0]).then(res => res.json());
+    const p = await fetch(s.homeworld).then(res => res.json());
+    return (
+      "Name: " +
+      n.name +
+      ", Title: " +
+      f.title +
+      ", Specie: " +
+      s.name +
+      ", Planet: " +
+      p.name
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// .then resolver værdien af promise. Hvis .then returner et promise så er det, 
+// det næste .then der resolver værdien
+// https://stackoverflow.com/questions/38884522/promise-pending
+let starWarsInfo = getPlanetforFirstSpeciesInFirstMovieForPersonAsync(1);
+starWarsInfo.then(function(result) {
+    console.log(result);
+})
+
+// Eks på hvis .then returner et promise så er det, 
+// det næste .then der resolver værdien
+/* 
+function initPromise() {
+  return new Promise(function(res,rej) {
+    res("initResolve");
+  })
+}
+initPromise().then(function(result) {
+    console.log(result); // "initResolve"
+    return new Promise(function(resolve, reject) {
+       setTimeout(function() {
+          resolve("secondPromise");
+       }, 1000)
+   })
+})
+.then(function(result) {
+    console.log(result); // "secondPromise"
+});
+*/
+
